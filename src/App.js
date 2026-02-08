f516e92c568ad3c9a989c087214efacc0420e095
csreddy89 <tseek2021@gmail.com>
2026-02-08 21:54:18 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useEffect } from "react";
+import axios from "axios";
 function App() {
+  const [message, setMessage] = useState("Loading...");
+  useEffect(() => {
+    axios.get("http://localhost:8080/api/hello")
+      .then(response => setMessage(response.data.message))
+      .catch(error => setMessage("Error fetching data"));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>React + Spring Boot</h1>
+      <p>{message}</p>
     </div>
-  );
-}
-
+); }
 export default App;
